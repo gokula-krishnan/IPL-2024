c46730a7be454df5f4b70c4ab48e1a4f69070982
GK <gokula_krishnan@trimble.com>
2024-04-07 16:53:08 +0530

    IPL-2024- hari draft rcb vs RR motm added

--- final_team_points.py
+++ final_team_points.py
@@ -38,33 +38,33 @@ for team in sortList:
     print(team)
 
 
-# Function to update Gist content
-def update_gist():
-    # Read JSON data from file
-    # Prepare Gist payload
-    payload = {
-        "files": {}
-    }
-
-    # Iterate through each key-value pair in JSON data and add to payload
-    payload["files"][os.getenv('gist_file_name')] = {"content": json.dumps(pointsData)}
-
-    # Prepare headers with access token
-    headers = {
-        "Authorization": f"Bearer {os.getenv('access_token')}",
-        "Accept": "application / vnd.github + json",
-        "X-GitHub-Api-Version": "2022-11-28",
-    }
-
-    # Make PATCH request to update Gist
-    response = requests.patch(f"https://api.github.com/gists/{os.getenv('gist_id')}", headers=headers, json=payload)
-
-    # Check if update was successful
-    if response.status_code == 200:
-        print("Gist content updated successfully.")
-    else:
-        print(f"Failed to update Gist. Status code: {response.status_code}")
-        print(f"Error message: {response.text}")
-
-
-update_gist()
+# # Function to update Gist content
+# def update_gist():
+#     # Read JSON data from file
+#     # Prepare Gist payload
+#     payload = {
+#         "files": {}
+#     }
+#
+#     # Iterate through each key-value pair in JSON data and add to payload
+#     payload["files"][os.getenv('gist_file_name')] = {"content": json.dumps(pointsData)}
+#
+#     # Prepare headers with access token
+#     headers = {
+#         "Authorization": f"Bearer {os.getenv('access_token')}",
+#         "Accept": "application / vnd.github + json",
+#         "X-GitHub-Api-Version": "2022-11-28",
+#     }
+#
+#     # Make PATCH request to update Gist
+#     response = requests.patch(f"https://api.github.com/gists/{os.getenv('gist_id')}", headers=headers, json=payload)
+#
+#     # Check if update was successful
+#     if response.status_code == 200:
+#         print("Gist content updated successfully.")
+#     else:
+#         print(f"Failed to update Gist. Status code: {response.status_code}")
+#         print(f"Error message: {response.text}")
+#
+#
+# update_gist()
